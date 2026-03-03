c9769490eaa452037f10a1cabe742584777854cb
Choubani Ghofrane <ghofrane.choubeni@esprit.tn>
2026-03-03 23:04:41 +0100

    Chap5

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -70,6 +70,12 @@ public class User implements Serializable {
 		this.id = id;
 	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 	public String getLastName() {
 		return lastName;
 	}

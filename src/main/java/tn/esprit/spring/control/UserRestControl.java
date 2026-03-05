644e24006871ba038c5c8e6d6328968ae90875ce
Choubani Ghofrane <ghofrane.choubeni@esprit.tn>
2026-03-05 11:36:12 +0100

    Fix UserRestControl constructor injection

--- src/main/java/tn/esprit/spring/control/UserRestControl.java
+++ src/main/java/tn/esprit/spring/control/UserRestControl.java
@@ -1,58 +1,42 @@
 package tn.esprit.spring.control;
 
+import tn.esprit.spring.services.IUserService;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.spring.entities.User;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import tn.esprit.spring.entities.User;
-import tn.esprit.spring.services.IUserService;
-
-// userRestControl
-@RestController // = @Controller + @ResponseBody 
+@RestController
 @RequestMapping("/user")
 public class UserRestControl {
 
-	//@Autowired 
-	//IUserService userService; 
+    private final IUserService userService;
 
-	
-	// URL : http://localhost:????/????/????/retrieve-all-users
-	@GetMapping("/retrieve-all-users")
-	public List<User> retrieveAllUsers() {
-		return userService.retrieveAllUsers();
-		//return list;
-	}
- 
-	// http://localhost:????/timesheet-devops/retrieve-user/{user-id}
-	@GetMapping("/retrieve-user/{user-id}")
-	public User retrieveUser(@PathVariable("user-id") String userId) {
-		return userService.retrieveUser(userId);
-	}
-	
-	 
+    public UserRestControl(IUserService userService) {
+        this.userService = userService;
+    }
 
-	// Ajouter User : http://localhost:????/timesheet-devops/add-user 
-	@PostMapping("/add-user")
-	public User addUser(@RequestBody User u) {
-		User user = userService.addUser(u); 
-		return user;
-	}
+    @GetMapping("/retrieve-all-users")
+    public List<User> retrieveAllUsers() {
+        return userService.retrieveAllUsers();
+    }
 
-	
-	// Supprimer User : 
-	// http://localhost:????/timesheet-devops/remove-user/{user-id}
-	@DeleteMapping("/remove-user/{user-id}") 
-	public void removeUser(@PathVariable("user-id") String userId) { 
-		userService.deleteUser(userId);
-	} 
+    @GetMapping("/retrieve-user/{user-id}")
+    public User retrieveUser(@PathVariable("user-id") Long userId) {
+        return userService.retrieveUser(userId);
+    }
 
-	// Modifier User 
-	// http://localhost:????/timesheet-devops/modify-user 
-	@PutMapping("/modify-user") 
-	public User updateUser(@RequestBody User user) {
-		return userService.updateUser(user);
-	}
-	 
-} 
- 
+    @PostMapping("/add-user")
+    public User addUser(@RequestBody User u) {
+        return userService.addUser(u);
+    }
+
+    @DeleteMapping("/remove-user/{user-id}")
+    public void removeUser(@PathVariable("user-id") Long userId) {
+        userService.removeUser(userId);
+    }
+
+    @PutMapping("/modify-user")
+    public User modifyUser(@RequestBody User u) {
+        return userService.modifyUser(u);
+    }
+}
